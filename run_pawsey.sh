29d0b7530e4b905e6cbb7df2c147b36cccd17680
Tom Harrop <twharrop@gmail.com>
2025-02-18 16:58:09 +1100

    blastdb

--- run_pawsey.sh
+++ run_pawsey.sh
@@ -32,23 +32,21 @@ fi
 # curl -Iv ftp.ncbi.nih.gov
 
 # run the pipeline with notemp to avoid re-downloading the data
-snakemake \
-    --profile profiles/pawsey_v8 \
-    --retries 1 \
-    --keep-going \
-    --notemp \
-    --cores 12 \
-    --local-cores "${SLURM_CPUS_ON_NODE}" \
-    -n
-
-exit 1
+# snakemake \
+#     --profile profiles/pawsey_v8 \
+#     --retries 1 \
+#     --keep-going \
+#     --notemp \
+#     --cores 12 \
+#     --local-cores "${SLURM_CPUS_ON_NODE}" \
+#     busco_databases_target
 
 # delete if everything is there, disabled for now
-# if [ $? -eq 0 ]; then
-#     printf "Deleting temp files"
-#     snakemake \
-#         --profile profiles/pawsey_v8 \
-#         --delete-temp-output \
-#         --cleanup-shadow \
-#         --local-cores "${SLURM_CPUS_ON_NODE}"
-# fi
+if [ $? -eq 0 ]; then
+    printf "Deleting temp files"
+    snakemake \
+        --profile profiles/pawsey_v8 \
+        --delete-temp-output \
+        --cleanup-shadow \
+        --local-cores "${SLURM_CPUS_ON_NODE}"
+fi
