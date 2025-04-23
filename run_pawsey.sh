bca19e5a89980e432138d6043ba095e466cc57e7
Tom Harrop <twharrop@gmail.com>
2025-04-23 11:34:49 +1000

    check busco DBs

--- run_pawsey.sh
+++ run_pawsey.sh
@@ -39,7 +39,7 @@ snakemake \
     --cores 12 \
     --notemp \
     --local-cores "${SLURM_CPUS_ON_NODE}" \
-    kraken2_build_db
+    busco_databases_target
 
 # delete if everything is there, disabled for now
 if [ $? -eq 0 ]; then
@@ -49,5 +49,5 @@ if [ $? -eq 0 ]; then
         --delete-temp-output \
         --cleanup-shadow \
         --local-cores "${SLURM_CPUS_ON_NODE}" \
-        kraken2_build_db
+        busco_databases_target
 fi
