e579eed315e811af9b3f65579ef2f16c558fb568
Tom Harrop <twharrop@gmail.com>
2025-07-31 14:39:54 +1000

    diamond proteomes

--- run_pawsey.sh
+++ run_pawsey.sh
@@ -39,7 +39,7 @@ snakemake \
     --cores 12 \
     --notemp \
     --local-cores "${SLURM_CPUS_ON_NODE}" \
-    upload_blast_db_files
+    diamond_makedb
 
 
 # delete if everything is there, disabled for now
@@ -50,6 +50,6 @@ if [ $? -eq 0 ]; then
         --delete-temp-output \
         --cleanup-shadow \
         --local-cores "${SLURM_CPUS_ON_NODE}" \
-        upload_blast_db_files
+        diamond_makedb
 fi
 
