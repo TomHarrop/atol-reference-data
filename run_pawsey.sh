d67d7c1718f59f78e52c0702c52afdd50d15570c
Tom Harrop <twharrop@gmail.com>
2025-08-11 15:07:36 +1000

    scatter gather for diamond

--- run_pawsey.sh
+++ run_pawsey.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #SBATCH --job-name=atol_refdata
-#SBATCH --partition=long
-#SBATCH --time=4-00
+##SBATCH --partition=long
+#SBATCH --time=0-02
 #SBATCH --cpus-per-task=2
 #SBATCH --ntasks=1
 #SBATCH --mem=8g
